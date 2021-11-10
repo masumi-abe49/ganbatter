a6206fa4e001ce8264ff1b72a53ae8de9861997e
masumi-abe49 <andfolk1129@gmail.com>
2021-11-11 00:04:08 +0900

    ユーザーログアウト機能実装

--- public_html/header.php
+++ public_html/header.php
@@ -26,9 +26,9 @@ require_once(__DIR__ .'/../config/config.php');
       <li><a href="<?= SITE_URL; ?>/">ホーム</a></li>
       <?php
       if(isset($_SESSION['me'])) { ?>
-      <li><a href="<?= SITE_URL; ?>/thread_all.php">一覧</a></li>
-      <li><a href="<?= SITE_URL; ?>/thread_favorite.php">お気に入り</a></li>
-      <li><a href="<?= SITE_URL; ?>/thread_create.php">作成</a></li>
+      <li><a href="<?= SITE_URL; ?>/ganbatta_create.php">作成</a></li>
+      <li><a href="<?= SITE_URL; ?>/ganbatta_all.php">頑張った！一覧</a></li>
+      <li><a href="<?= SITE_URL; ?>/ganbatta_likes.php">いいぞ！一覧</a></li>
       <?php } else { ?>
         <li class="user-btn"><a href="<?= SITE_URL; ?>/login.php">ログイン</a></li>
         <li><a href="<?= SITE_URL; ?>/signup.php">ユーザー登録</a></li>
@@ -38,6 +38,9 @@ require_once(__DIR__ .'/../config/config.php');
   <div class="header-r">
     <?php
       if(isset($_SESSION['me'])) { ?>
+      <div class="prof-show">
+        <a href="<?= SITE_URL; ?>/mypage.php"><span class="name"><?= h($_SESSION['me']->username); ?></span></a>
+      </div>
       <form action="logout.php" method="post" id="logout" class="user-btn">
         <input type="submit" value="ログアウト">
         <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
---
+++ public_html/logout.php
@@ -0,0 +1,4 @@
+<?php
+require_once(__DIR__ .'/header.php');
+$app = new ganbatter\Controller\Logout();
+$app->run();
